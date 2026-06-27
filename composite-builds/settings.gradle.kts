047bcdb45bbae0f2474e121634e431dc8483e786
CODE-IDE <sagineavwurinrinkoghene1@gmail.com>
2026-06-27 09:27:05 +0100

    fix: add dependencyResolutionManagement repos to composite-builds settings

--- composite-builds/settings.gradle.kts
+++ composite-builds/settings.gradle.kts
@@ -6,6 +6,16 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
+        maven { url = uri("https://s01.oss.sonatype.org/content/groups/public/") }
+    }
+}
+
 rootProject.name = "build-deps"
 
 include(":java-compiler")
